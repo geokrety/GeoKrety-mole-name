f82be9718e3254ecfe9dcd7d6dc3e985f999eb7d
Mathieu Alorent <github@kumy.net>
2017-11-07 21:44:44 +0100

    Add blueprint

--- app/main.py
+++ app/main.py
@@ -9,7 +9,24 @@ from flask_babel import Babel
 from ihih import IHIH
 from sendmail import sendConfirmation, sendProposition, sendValidatedProposition
 
+# https://stackoverflow.com/a/36033627/944936
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
 app = Flask(__name__)
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/molename')
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
